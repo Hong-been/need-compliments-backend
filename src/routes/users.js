ff955b86dbd75befabb018dee0d21b029bb59d3d
HongBeen <redbean-2@naver.com>
2022-05-22 02:13:33 +0900

    refactor: try-catch

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,34 +3,38 @@ import {User} from "../models/user";
 
 const router = Router();
 
-router.get("/userid/:userid", (req, res) => {
-	User.findOneByUserId(req.params.userid)
-		.then((user) => {
-			if (!user)
-				return res.status(404).send({succes: false, err: "User not found"});
-			res.send(`findOne successfully: ${user}`);
-		})
-		.catch((err) => res.status(500).send(err));
+router.get("/userid/:userid", async (req, res) => {
+	try {
+		const result = await User.findOneByUserId(req.params.userid);
+		if (!result) {
+			return res.status(404).send({succes: false, err: "User not found"});
+		}
+		res.send(`findOne successfully: ${result}`);
+	} catch (err) {
+		res.status(500).send(err);
+	}
 });
 
 router.post("/", async (req, res) => {
 	try {
 		const result = await User.create(req.body);
-		res.sendStatus(200);
+		return res.sendStatus(200);
 	} catch (err) {
 		if (err.name === "MongoServerError" && err.code === 11000) {
 			console.log(err);
-			res.status(422).send({
+			return res.status(422).send({
 				succes: false,
 				message: `user already exists. : duplicate ${Object.keys(
 					err.keyValue
 				).join(",")}`,
 			});
-		} else if (err.name === "ValidationError") {
-			res.status(400).send({succes: false, message: err.message});
-		} else {
-			res.status(500).send(err);
 		}
+
+		if (err.name === "ValidationError") {
+			return res.status(400).send({succes: false, message: err.message});
+		}
+
+		res.status(500).send(err);
 	}
 });
 
@@ -38,10 +42,12 @@ router.delete("/userid/:userid", async (req, res) => {
 	try {
 		const result = await User.deleteByUserId(req.params.userid);
 		if (!result) {
-			res.status(404).send({succes: false, message: "user not found!"});
-		} else res.sendStatus(200);
+			return res.status(404).send({succes: false, message: "user not found!"});
+		}
+
+		return res.sendStatus(200);
 	} catch (err) {
-		res.status(500).send(err);
+		return res.status(500).send(err);
 	}
 });
 
