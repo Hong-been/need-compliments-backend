73b50abdd3e0029698f092bed22b0a84577b5c53
HongBeen <redbean-2@naver.com>
2022-05-22 20:45:13 +0900

    add:cors option

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
@@ -12,6 +13,22 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+const whiteList = [
+	"http://localhost:3000/",
+	"https://need-compliments-sandy.vercel.app/",
+];
+const corsOption = {
+	origin: (origin, callback) => {
+		if (whiteList.indexOf(origin) > -1) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not Allowed origin!"));
+		}
+	},
+};
+app.use(cors(corsOption));
+
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 
