f1218ba2d4f0541e443a0dc1e0ff3e0623273dfc
HongBeen <redbean-2@naver.com>
2022-05-23 19:03:43 +0900

    add: postman undefined origin

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const whiteList = [
 ];
 const corsOption = {
 	origin: (origin, callback) => {
-		if (whiteList.indexOf(origin) > -1) {
+		if (!origin && whiteList.indexOf(origin) > -1) {
 			callback(null, true);
 		} else {
 			callback(new Error(`Not Allowed origin! : ${origin}`));
